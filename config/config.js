0c06b34dd53b044dbc38ffe748c4d330a3e9244c
Diana Avanesian <dianaskybin@gmail.com>
2021-08-13 17:20:30 -0400

    fixing gitignore

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const configurations = {
+    'db': 'mongodb+srv://userFORtesting:userFORtesting@cluster0.rfx4x.mongodb.net/passmgr',
+    'github':{
+        'clientId': '3551f01eda1714d6978e',
+        'clientSecret': '3882821b02b4302c1f64d02c9595d8b48b565eee',
+        'callbackUrl': 'http://localhost:3000/github/callback'
+    }
+}
+
+module.exports = configurations;
